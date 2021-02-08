08ecc58e8daf54048dc8685fbbedddd51212190e
bushbass <alex.nielsen@gmail.com>
2021-02-08 01:58:27 -0500

    need to review end of 15

--- sick-fits/backend/keystone.ts
+++ sick-fits/backend/keystone.ts
@@ -1,6 +1,12 @@
+import { createAuth } from '@keystone-next/auth';
 import { config, createSchema } from '@keystone-next/keystone/schema';
 import 'dotenv/config';
 import { User } from './schemas/Users';
+import { Product } from './schemas/Product';
+import {
+  withItemData,
+  statelessSessions,
+} from '@keystone-next/keystone/session';
 
 const databaseURL =
   process.env.DATABASE_URL || 'mongodb://localhost/eystone-sick-fits-tutorial';
@@ -10,25 +16,44 @@ const sessionConfig = {
   secret: process.env.COOKIE_SECRET,
 };
 
-export default config({
-  server: {
-    cors: {
-      origin: [process.env.FRONTEND_URL],
-      credential: true,
-    },
+const { withAuth } = createAuth({
+  listKey: 'User',
+  identityField: 'email',
+  secretField: 'password',
+  initFirstItem: {
+    fields: ['name', 'email', 'password'],
+    // todo add in initial roles here
   },
-  db: {
-    adapter: 'mongoose',
-    url: databaseURL,
-    // TODO: Add data seeding here
-  },
-  lists: createSchema({
-    // schema itmes go in here
-    User,
-  }),
-  ui: {
-    // change for roles
-    isAccessAllowed: () => true,
-  },
-  // todo add session valuse here
 });
+
+export default withAuth(
+  config({
+    server: {
+      cors: {
+        origin: [process.env.FRONTEND_URL],
+        credential: true,
+      },
+    },
+    db: {
+      adapter: 'mongoose',
+      url: databaseURL,
+      // TODO: Add data seeding here
+    },
+    lists: createSchema({
+      // schema itmes go in here
+      User,
+      Product,
+    }),
+    ui: {
+      // show ui only for people who pass this test
+      isAccessAllowed: ({ session }) => {
+        // console.log(session);
+        return !!session?.data;
+      },
+    },
+    //graphql query
+    session: withItemData(statelessSessions(sessionConfig), {
+      User: `id`,
+    }),
+  })
+);
---
+++ sick-fits/backend/schemas/Product.ts
@@ -0,0 +1,26 @@
+import { text, select } from '@keystone-next/fields';
+import { list } from '@keystone-next/keystone/schema';
+export const Product = list({
+  //todo
+  // access:
+  fields: {
+    name: text({ isRequired: true }),
+    description: text({
+      ui: {
+        displayMode: 'textarea',
+      },
+    }),
+    status: select({
+      options: [
+        { label: 'Draft', value: 'DRAFT' },
+        { label: 'Available', value: 'AVAILABLE' },
+        { label: 'AUnavailable', value: 'UNAVAILABLE' },
+      ],
+      defaultValue: 'DRAFT',
+      ui: {
+        displayMode: 'segmented-control',
+        createView: { fieldMode: 'hidden' },
+      },
+    }),
+  },
+});
