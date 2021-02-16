8f1882c8f21d06b18af538baf7e42106cdd2ad38
bushbass <alex.nielsen@gmail.com>
2021-02-16 01:13:49 -0500

    finished 20

--- sick-fits/frontend/pages/index.js
+++ sick-fits/frontend/pages/index.js
@@ -1,10 +1,1 @@
-export default function IndexPage(props) {
-  return (
-    <>
-      <h2>index page</h2>
-      <p>Hello!</p>
-      <code>code</code>
-      <p>I am a child</p>
-    </>
-  );
-}
+export { default } from './products';
