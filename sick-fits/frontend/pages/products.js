d63e1be732effffd00a6bd15777a0fe3c4911056
bushbass <alex.nielsen@gmail.com>
2021-02-26 10:36:10 -0500

    finished 32

--- sick-fits/frontend/pages/products.js
+++ sick-fits/frontend/pages/products.js
@@ -1,9 +1,12 @@
+import Pagination from '../components/Pagination';
 import Products from '../components/Products';
 
 export default function ProductsPage() {
   return (
     <div>
+      <Pagination page={6} />
       <Products />
+      <Pagination page={1} />
     </div>
   );
 }
