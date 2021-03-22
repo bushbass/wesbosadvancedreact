0b4ea734832cdf26d5a7acc7148034672e11afc1
bushbass <alex.nielsen@gmail.com>
2021-03-22 09:30:07 -0400

    finished 49

---
+++ sick-fits/frontend/components/RemoveFromCart.js
@@ -0,0 +1,38 @@
+import { useMutation } from '@apollo/client';
+import { gql } from 'graphql-tag';
+import { remove } from 'nprogress';
+import styled from 'styled-components';
+
+const BigButton = styled.button`
+  font-size: 3rem;
+  background: none;
+  border: 0;
+  &:hover {
+    color: var(--red);
+    cursor: pointer;
+  }
+`;
+const REMOVE_FROM_CART_MUTATION = gql`
+  mutation REMOVE_FROM_CART_MUTATION($id: ID!) {
+    deleteCartItem(id: $id) {
+      id
+    }
+  }
+`;
+
+function RemoveFromCart({ id }) {
+  const [removeFromCart, { loading }] = useMutation(REMOVE_FROM_CART_MUTATION, {
+    variables: { id: id },
+  });
+  return (
+    <BigButton
+      onClick={removeFromCart}
+      type="button"
+      title="Reomove this item from the cart"
+    >
+      &times;
+    </BigButton>
+  );
+}
+
+export default RemoveFromCart;
